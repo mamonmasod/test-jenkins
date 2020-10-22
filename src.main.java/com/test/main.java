e099b2cfb397d4e02dfbaf4ecc1caf61d5da64e1
mamonmasod@gmail.com <Mamonthebest4241126>
2020-10-22 12:48:15 +0300

    first commit

---
+++ src.main.java/com/test/main.java
@@ -0,0 +1,14 @@
+package com.test;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by hp on 10/22/2020.
+ */
+public class main {
+    private static final Logger logger = Logger.getLogger(main.class.getName());
+
+    public static void main(String[] args) {
+        logger.log(Level.INFO, "test");
+    }
+}
